724c0d20fb19286f7ace3b08be9f1c9cd9dc9b94
wendy60113 <wendy60113@gmail.com>
2023-08-08 23:04:58 +0800

    add intro page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import HomePage from '@site/src/components/HomePage';
+import IntroPage from '@site/src/components/IntroPage';
 
-import styles from './index.module.css';
+// import styles from './index.module.css';
 
 export default function Home() {
   return (
     <Layout>
       <main>
         <HomePage />
+        <IntroPage/>
       </main>
     </Layout>
   );
