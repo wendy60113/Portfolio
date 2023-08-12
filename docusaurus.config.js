bb1355ca1fa412fe4bfa6174f99e9c90d4e6fcae
wendy60113 <wendy60113@gmail.com>
2023-08-12 12:02:10 +0800

    add BlogPostItem examplge

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,15 +44,15 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+				blog: {
+					showReadingTime: true,
+					postsPerPage: 3,
+				},
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss:[
+            require.resolve('./src/css/custom.css'),
+          ], 
+          
         },
       }),
     ],
---
+++ src/theme/BlogListPage/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import BlogLayout from '@theme/BlogLayout';
+import BlogListPaginator from '@theme/BlogListPaginator';
+import BlogPostItems from '@theme/BlogPostItems';
+import style from './index.module.css'
+
+function BlogListPageContent(props) {
+	const { metadata, items } = props;
+
+	return (
+		<BlogLayout>
+			<div className={style.BlogPostItems}>
+				<BlogPostItems items={items} />
+			</div>
+			<BlogListPaginator metadata={metadata} />
+		</BlogLayout>
+	);
+}
+
+export default function BlogListPage(props) {
+	return (
+        <>
+            <BlogListPageContent {...props} />
+        </>
+	);
+}
