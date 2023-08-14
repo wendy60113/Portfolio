6aba89496e855a3e1300d4bf21b43b7f0072b8f1
wendy60113 <wendy60113@gmail.com>
2023-08-14 23:55:36 +0800

    edit BlogPostPage

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import BlogLayout from '@theme/BlogLayout';
+import { MDXProvider } from "@mdx-js/react"
+import MDXComponents from "@theme/MDXComponents"
+
+
 // import BlogListPaginator from '@theme/BlogListPaginator'
 
 // import BlogPostItems from '@theme/BlogPostItem';
 function BlogPostPageContent(props) {
-	const { content } = props;
-    const { metadata } = content
-	console.log(metadata)
+	const { content:BlogPostContents } = props;
+	// const { metadata } = BlogPostContents
+		console.log(props)
 	return (
 		<BlogLayout>
 			{/* <div>
 				<BlogPostItems items={items} />
 			</div>
 			<BlogListPaginator metadata={metadata} /> */}
+			<article>
+			<MDXProvider components={MDXComponents}>
+				<BlogPostContents />
+			</MDXProvider>
+			</article>
 		</BlogLayout>
 	);
 }
