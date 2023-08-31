b58cf4f22df79becddb2767c7dba853ac40ff65f
wendy60113 <wendy60113@gmail.com>
2023-09-01 00:10:34 +0800

    edit proj

--- config/blog.js
+++ config/blog.js
@@ -1,51 +1,36 @@
 module.exports = [
     {
-        slug: "test",
-        title: "專案名稱",
-        author: {
-            name: "Gao Wei",
-            title: "Docusaurus Core Team",
-            url: "https://github.com/wgao19",
-            image_url: "https://github.com/wgao19.png"
-        },
-        test_image: "/img/docusaurus.png",
-        tags: [
+        slug: "NPM-巢狀選單元件",
+        title: "NPM-巢狀選單元件",
+        test_image: "/img/cover/nesteddropdown01.jpg",
+        tags:[
             "前端程式",
-            "網頁設計",
-            "資料庫設計"
+            "npm上架",
+            "UI/UX設計"
         ],
-        url:'/blog/test'
+        url:'/blog/NPM-巢狀選單元件'
     },
     {
-        slug: "test",
-        title: "test",
-        author: {
-            name: "Gao Wei",
-            title: "Docusaurus Core Team",
-            url: "https://github.com/wgao19",
-            image_url: "https://github.com/wgao19.png"
-        },
-        test_image: "/img/docusaurus.png",
-        tags: [
-            "前端程式",
-            "網頁設計"
-        ],
-        url:'/blog/test'
-    },
-    {
-        slug: "test",
-        title: "test",
-        author: {
-            name: "Gao Wei",
-            title: "Docusaurus Core Team",
-            url: "https://github.com/wgao19",
-            image_url: "https://github.com/wgao19.png"
-        },
-        test_image: "/img/docusaurus.png",
+        slug: "咖啡廳搜尋網頁",
+        title: "咖啡廳搜尋網頁",
+        test_image: "/img/cover/cafelist_01.jpg",
         tags: [
             "資料庫設計",
+            "前端程式",
+            "UI/UX設計",
             "網頁設計"
         ],
-        url:'/blog/test'
+        url:'/blog/咖啡廳搜尋網頁'
+    },
+    {
+        slug: "測驗網頁",
+        title: "測驗網頁",
+        test_image: "/img/cover/Exam.jpg",
+        tags: [
+            "前端程式",
+            "UI/UX設計",
+            "網頁設計"
+        ],
+        url:'/blog/測驗網頁'
     }
   ];
